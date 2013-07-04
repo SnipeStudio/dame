320715694c95687cd62f7e96e826b2a53f1983e1
Anton Mironov <98mirant@gmail.com>
2013-07-04 21:37:09 +0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -4,37 +4,55 @@
 #include <stdlib.h>
 using namespace std;
 int main(int argc,char* argv[])
+
 {
 char* path=new char[1000];
+char* mode=new char[20];
+cout<<"Enter mode: (d|m|b)"<<endl;
+cin>>mode;
+if(*mode!='d'&&*mode!='m'&&*mode!='b')
+{
+cout<<"Not valid mode :"<<*mode<<endl;
+return 1;
+}
+if(*mode=='d'||*mode=='b')
+{
 if(argc<2)
 {
-cout<<"Enter path to spammed directory"<<endl;
+cout<<"Enter path to spammed file"<<endl;
 cin>>path;
 }
 else
 strcpy(path,argv[1]);
+}
 ofstream ofs;
 ofs.open(path,ios::app|ios::out);
-if(ofs)
+
+if(ofs||*mode=='m')
 {
 void *m;
 while(true)
 {
+if(*mode=='m'||*mode=='b')
+{
 m = malloc(1024*1024);
 memset(m,0,1024*1024);
 m = malloc(1024*1024);
 memset(m,0,1024*1024);
-ofs<<"The Rite of Spring is a ballet and orchestral concert work by the Russian composer Igor Stravinsky. It was written for the 1913Â Paris season of Sergei Dighilev's Ballets Russes company, with choreography by Vaslav Nijinsky and staeesigns and costumes by Nicholas Roerich";
-ofs<<"The Rite of Spring is a ballet and orchestral concert work by the Russian composer Igor Stravinsky. It was written for the 1913Â Paris season of Sergei Dihilev's Ballets Russes company, with choreography by Vaslav Nijinsky and stag signs and costumes by Nicholas Roerich";
-ofs<<"The Rite of Spring is a ballet and orchestral concert work by the Russian composer Igor Stravinsky. It was written for the 1913Â Paris season of Sergei Dihilev's Ballets Russes company, with choreography by Vaslav Nijinsky and stag signs and costumes by Nicholas Roerich";
-ofs<<"The Rite of Spring is a ballet and orchestral concert work by the Russian composer Igor Stravinsky. It was written for the 1913Â Paris season of Sergei ihilev's Ballets Russes company, with choreography by Vaslav Nijinsky and stag esigns and costumes by Nicholas Roerich";
-ofs<<"The Rite of Spring is a ballet and orchestral concert work by the Russian composer Igor Stravinsky. It was written for the 1913Â Paris season of Sergei iaghilev's Ballets Russes company, with choreography by Vaslav Nijinsky and stg designs and costumes by Nicholas Roerich";
+}
+if(*mode=='d'||*mode=='b')
+{
+ofs<<"The Rite of Spring is a ballet and orchestral concert work by the Russian composer Igor Stravinsky. It was written for the 1913 Paris season of Sergei Dighilev's Ballets Russes company, with choreography by Vaslav Nijinsky and staeesigns and costumes by Nicholas Roerich"<<endl;
+ofs<<"The Rite of Spring is a ballet and orchestral concert work by the Russian composer Igor Stravinsky. It was written for the 1913 Paris season of Sergei Dighilev's Ballets Russes company, with choreography by Vaslav Nijinsky and staeesigns and costumes by Nicholas Roerich"<<endl;
+ofs<<"The Rite of Spring is a ballet and orchestral concert work by the Russian composer Igor Stravinsky. It was written for the 1913 Paris season of Sergei Dighilev's Ballets Russes company, with choreography by Vaslav Nijinsky and staeesigns and costumes by Nicholas Roerich"<<endl;
+ofs<<"The Rite of Spring is a ballet and orchestral concert work by the Russian composer Igor Stravinsky. It was written for the 1913 Paris season of Sergei Dighilev's Ballets Russes company, with choreography by Vaslav Nijinsky and staeesigns and costumes by Nicholas Roerich"<<endl;
+}
 };
-
 ofs.close();
 }
 
 cout<<path<<endl;
 delete[] path;
+delete mode;
 return 0;
 }
