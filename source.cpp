722ccf1cd9f996d1b2353c72dc586d7eb998dd48
Anton Mironov <98mirant@gmail.com>
2013-07-05 18:38:06 +0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -3,90 +3,126 @@
 #include <cstring>
 #include <stdlib.h>
 #include <sys/stat.h>
-
-
-long int getFileSize(const char * fileName)
-{
-    struct stat file_stat;
-    stat(fileName, &file_stat);
-    return file_stat.st_size;
-}
+#include <time.h>
 using namespace std;
+
 int main(int argc,char* argv[])
 {
-char* path=new char[1000];
-char* mode=new char[20];
-char mult='b';
-long int limit=0,size=0;
-cout<<"Enter mode: (d|m|b|dl|bl)"<<endl;
-cin>>mode;
-if(strcmp(mode,"d")&&strcmp(mode,"m")&&strcmp(mode,"b")&&strcmp(mode,"dl")&&strcmp(mode,"bl"))//d-disk eat only; m - memory eat only; b - disk and memory eat
-{
-cout<<"Not valid mode :"<<*mode<<endl;
-return 1;
+        
+        char* path=new char[1000];
+        char* mode=new char[20];
+        char mult='b';
+        long int limit=0,size=0;
+        cout<<"Enter mode: (d|m|b|dl|bl)"<<endl;
+        cin>>mode;
+        if(strcmp(mode,"d")&&strcmp(mode,"m")&&strcmp(mode,"b")&&strcmp(mode,"dl")&&strcmp(mode,"bl"))//d-disk eat only; m - memory eat only; b - disk and memory eat
+        {
+                cout<<"Not valid mode :"<<*mode<<endl;
+                return 1;
+        }
+        if(!strcmp(mode,"d")||!strcmp(mode,"b"))
+        {
+                if(argc<2)
+                {
+                        cout<<"Enter path to spammed file"<<endl;
+                        cin>>path;
+                }
+                else
+                strcpy(path,argv[1]);
+        }
+        if(!strcmp(mode,"dl")||!strcmp(mode,"bl"))
+        {
+                if(argc<2)
+                {
+                        cout<<"Enter path to spammed file"<<endl;
+                        cin>>path;
+                }
+                else
+                        strcpy(path,argv[1]);
+        cout<<"Enter limit:"<<endl;
+        cin>>limit>>mult;
+        if(mult=='b')
+                limit*=1;
+        if(mult=='k')
+                limit*=1024;
+        if(mult=='m')
+                limit*=1024*1024;
+        if(mult=='g')
+                limit*=1024*1024*1024;
+        cout<<limit<<endl;
+        }
+        else
+                strcpy(path,argv[1]);
+double start = clock ();
+FILE *fp1 = fopen(path,"ab");
+FILE *fp2 = fopen(strcat(path,"2"),"ab");
+FILE *fp3 = fopen(strcat(path,"3"),"ab");
+        if((fp1&&fp2&&fp3)||!strcmp(mode,"m"))
+        {
+                void *m;
+                
+                while(true)
+                {
+                        if(!strcmp(mode,"m")||!strcmp(mode,"b")||!strcmp(mode,"bl"))
+                        {
+                                m = malloc(1024*1024);
+                                memset(m,0,1024*1024);
+                        }
+                        if(!strcmp(mode,"d")||!strcmp(mode,"b")||!strcmp(mode,"dl")||!strcmp(mode,"bl"))
+                        {
+                                if((ftell(fp1)+ftell(fp2)+ftell(fp2))<limit)
+                                {
+                                        fseek(fp1,0,SEEK_END);
+                                        long size1 = ftell(fp1);
+    				 fseek(fp2,0,SEEK_END);
+                                        long size2 = ftell(fp2);
+					 fseek(fp3,0,SEEK_END);
+                                        long size3 = ftell(fp3);
+                                        if(limit<=1073741824)
+                                        {
+                                        char *buffer = (char*)calloc(limit/3-size1,1);
+                                        fwrite(buffer,limit-size,1,fp1);
+					delete[] buffer;
+					buffer = (char*)calloc(limit/3-size2,1);
+                                        fwrite(buffer,limit-size,1,fp2);
+					delete[] buffer;
+					buffer = (char*)calloc(limit/3-size3,1);
+                                        fwrite(buffer,limit-size,1,fp3);
+					delete[] buffer;
+                                        }
+                                        if(limit>1073741824)
+                                        {
+                                         long int count=limit/1073741824;
+                                        limit/=count;
+                                        for(int i=0;i<count;i++)
+                                        {
+                                        char *buffer = (char*)calloc(limit/3-size1,1);
+                                        fwrite(buffer,limit-size,1,fp1);
+					delete[] buffer;
+					buffer = (char*)calloc(limit/3-size2,1);
+                                        fwrite(buffer,limit-size,1,fp2);
+					delete[] buffer;
+					buffer = (char*)calloc(limit/3-size3,1);
+                                        fwrite(buffer,limit-size,1,fp3);
+					delete[] buffer;
+                                        }
+                                        }
+                                }
+                                else
+                                {       
+                                        break;
+                                }
+                        }
+                };
+                if(!strcmp(mode,"d")||!strcmp(mode,"dl")||!strcmp(mode,"bl"))
+                      {  fclose(fp1);
+fclose(fp2);
+fclose(fp3);}
+        }
+double stop = clock();
+double total = (stop - start) / CLOCKS_PER_SEC;
+        cout<<"Done for "<<total<<" sec"<<endl;
+        delete[] path;
+        delete mode;
+        return 0;
 }
-if(!strcmp(mode,"d")||!strcmp(mode,"b"))
-{
-if(argc<2)
-{
-cout<<"Enter path to spammed file"<<endl;
-cin>>path;
-}
-else
-strcpy(path,argv[1]);
-}
-if(strcmp(mode,"dl")||strcmp(mode,"bl"))
-{
-if(argc<2)
-{
-cout<<"Enter path to spammed file"<<endl;
-cin>>path;
-cout<<"Enter limit in bytes"<<endl;
-cin>>limit>>mult;
-if(mult=='b')
-limit*=1;
-if(mult=='k')
-limit*=1024;
-if(mult=='m')
-limit*=1024*1024;
-if(mult=='g')
-limit*=1024*1024*1024;
-cout<<limit;
-}
-else
-strcpy(path,argv[1]);
-
-}
-ofstream ofs;
-ofs.open(path,ios::app|ios::out);
-if(ofs||!strcmp(mode,"m"))
-{
-void *m;
-while(true)
-{
-if(!strcmp(mode,"dl")||!strcmp(mode,"bl"))
-{
-size=getFileSize(path);
-if(size>limit)
-break;
-}
-if(!strcmp(mode,"m")||!strcmp(mode,"b"))
-{
-m = malloc(1024*1024);
-memset(m,0,1024*1024);
-}
-if(!strcmp(mode,"d")||!strcmp(mode,"b")||!strcmp(mode,"dl")||!strcmp(mode,"bl"))
-{
-ofs<<"The Rite of Spring is a ballet and orchestral concert work by the Russian composer Igor Stravinsky. It was written for the 1913 Paris season of Sergei Dighilev's Ballets Russes company, with choreography by Vaslav Nijinsky and staeesigns and costumes by Nicholas Roerich"<<endl;
-}
-
-};
-ofs.close();
-}
-
-cout<<"<<endl;
-delete[] path;
-delete mode;
-return 0;
-}
-
