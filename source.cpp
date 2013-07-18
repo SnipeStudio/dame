d952d401a55c3943f2a4823643853b89305e79f0
Anton Mironov <98mirant@gmail.com>
2013-07-18 13:03:13 +0400

    Updating source file delete writing functions from main()

--- source.cpp
+++ source.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 char ver[6]="2.3";
 char name[35]="Disk Drive and Memory Eater(dame)";
-char vermod[10]="0.3";
+char vermod[20]="0.4-beta";
 int man()
 {
 char line[81]="+------------------------------------------------------------------------------+";
@@ -20,12 +20,10 @@ cout<<"| dame ed    -           start to eat disk drive space in a specific fold
 cout<<"| USAGE: dame ed /path/to/eat                                                  |"<<endl;
 cout<<"| dame edl    -          start to eat disk drive space with limits             |"<<endl;
 cout<<"| USAGE: dame edl /path/to/eat spacetoeat {b|k|m|g}                            |"<<endl;
-cout<<"| dame em    -           start to eat memory                                   |"<<endl;
+cout<<"| dame em    -           start to eat memory without limits                    |"<<endl;
 cout<<"| dame eml   -           start to eat memory with limits                       |"<<endl;
 cout<<"| USAGE: dame eml memorytoeat {b|k|m|g}                                        |"<<endl;
-cout<<"| dame emd    -          start to eat memory and disk drive space              |"<<endl;
-cout<<"| dame emdl    -         start to eat memory and disk drive space with limits  |"<<endl;
-cout<<"| dame emlr - start to eat memory with limits,timeopt,rate                     |"<<endl;
+cout<<"| dame emlr - start to eat memory with limits,time options and rate            |"<<endl;
 cout<<"| USAGE: dame emlr memorytoeat {b|k|m|g} timeopt ratepertime {b|k|m|g}         |"<<endl;
 cout<<line<<endl;
 return 0;
@@ -34,7 +32,7 @@ return 0;
 int eMl(char* limit,char* mult)
 {
 	unsigned long long limit_long=atol(limit),memory_used=0;
-	 if(!strcmp(mult,"b"))
+                if(!strcmp(mult,"b"))
                 	limit_long*=1;
        		if(!strcmp(mult,"k"))
                 	limit_long*=1024;
@@ -61,6 +59,116 @@ int em()
 	}
 	while(true){};
 	return 0;
+}
+
+int edl(char* path,char* limit,char* mult)
+{
+    	unsigned long long limit_long=atol(limit);
+        if(!strcmp(mult,"b"))
+            limit_long*=1;
+        if(!strcmp(mult,"k"))
+            limit_long*=1024;
+        if(!strcmp(mult,"m"))
+            limit_long*=1024*1024;
+        if(!strcmp(mult,"g"))
+            limit_long*=1024*1024*1024;
+        FILE *fp1 = fopen(path,"ab");
+        FILE *fp2 = fopen(strcat(path,"2"),"ab");
+        FILE *fp3 = fopen(strcat(path,"3"),"ab"); 
+        if(fp1&&fp2&&fp3)
+        {
+            long long fSizeS=0,fSizeF=fSizeS;
+            while(fSizeF<limit_long)
+            {
+                fSizeS=ftell(fp1)+ftell(fp2)+ftell(fp2);
+                char *buffer = (char*)calloc(1024,1);
+                fwrite(buffer,1024,1,fp1);
+                delete[] buffer;
+                buffer = (char*)calloc(1024,1);
+                fwrite(buffer,1024,1,fp2);
+                delete[] buffer;
+                buffer = (char*)calloc(1024,1);
+                fwrite(buffer,1024,1,fp3);
+                delete[] buffer;
+                long long fSizeF=ftell(fp1)+ftell(fp2)+ftell(fp2);
+                if(fSizeF==fSizeS&&fSizeF<limit_long)
+                {
+                    cout<<"There are some errors during writing file"<<endl;
+                    return -1;
+                }
+            }
+            fclose(fp1);
+            fclose(fp2);
+            fclose(fp3);
+        }
+        return 0;
+}
+int emd(char* path)
+{
+    	void *m;
+        FILE *fp1 = fopen(path,"ab");
+        FILE *fp2 = fopen(strcat(path,"2"),"ab");
+        FILE *fp3 = fopen(strcat(path,"3"),"ab");  
+	while(m = malloc(1024))
+	{
+                unsigned long long fSizeS=ftell(fp1)+ftell(fp2)+ftell(fp2);
+                fseek(fp1,0,SEEK_END);
+                fseek(fp2,0,SEEK_END);
+                fseek(fp3,0,SEEK_END);
+                char *buffer = (char*)calloc(1024,1);
+                fwrite(buffer,1024,1,fp1);
+                delete[] buffer;
+                buffer = (char*)calloc(1024,1);
+                fwrite(buffer,1024,1,fp2);
+                delete[] buffer;
+                buffer = (char*)calloc(1024,1);
+                fwrite(buffer,1024,1,fp3);
+                delete[] buffer;
+                long long fSizeF=ftell(fp1)+ftell(fp2)+ftell(fp2);
+                if(fSizeF==fSizeS)
+                {
+                    cout<<"There are some errors during eating your disk drive"<<endl;
+                    break;
+                }
+            memset(m,0,1024);
+	}
+	while(true){};
+	return 0;
+}
+int ed(char* path)
+{
+        FILE *fp1 = fopen(path,"ab");
+        FILE *fp2 = fopen(strcat(path,"2"),"ab");
+        FILE *fp3 = fopen(strcat(path,"3"),"ab");  
+        if(fp1&&fp2&&fp3)
+        {
+            while(true)
+            {
+                long long fSizeS=ftell(fp1)+ftell(fp2)+ftell(fp2);
+                fseek(fp1,0,SEEK_END);
+                fseek(fp2,0,SEEK_END);
+                fseek(fp3,0,SEEK_END);
+                char *buffer = (char*)calloc(1024,1);
+                fwrite(buffer,1024,1,fp1);
+                delete[] buffer;
+                buffer = (char*)calloc(1024,1);
+                fwrite(buffer,1024,1,fp2);
+                delete[] buffer;
+                buffer = (char*)calloc(1024,1);
+                fwrite(buffer,1024,1,fp3);
+                delete[] buffer;
+                long long fSizeF=ftell(fp1)+ftell(fp2)+ftell(fp2);
+                if(fSizeF==fSizeS)
+                {
+                    cout<<"There are some errors during eating your disk drive"<<endl;
+                    break;
+                }
+           }
+           fclose(fp1);
+           fclose(fp2);
+           fclose(fp3);
+        }
+        return 0;
 }
 
 int eMLR(char* limit, char* timeopt, char* rate, char* multSpace, char* multRate)
@@ -152,20 +260,17 @@ int main(int argc,char** argv)
                 man();
                 return 0;
         }
-        char* path=new char[1000];
+        double start = clock ();
         char* mode=new char[20];
         strcpy(mode,argv[1]);
-        char* mult=new char[10];
-        char* buf=new char[3000];
-        long int limit=0,size=0;
-        if(strcmp(mode,"ed")&&strcmp(mode,"edl")&&strcmp(mode,"emd")&&strcmp(mode,"emdl")&&strcmp(mode,"edl")&&strcmp(mode,"ver")&&strcmp(mode,"man")&&strcmp(mode,"eml")&&strcmp(mode,"emlr"))
+        if(strcmp(mode,"ed")&&strcmp(mode,"edl")&&strcmp(mode,"emd")&&strcmp(mode,"edl")&&strcmp(mode,"ver")&&strcmp(mode,"man")&&strcmp(mode,"eml")&&strcmp(mode,"emlr"))
         {
                 cout<<"Not valid mode :"<<mode<<endl;
                 return 1;
         }
-        if(!strcmp(mode,"ed")||!strcmp(mode,"edm"))
+        if(!strcmp(mode,"ed"))
         {
-                        strcpy(path,argv[2]);
+            ed(argv[3]);
         }
 	if(!strcmp(mode,"man"))
 		man();
@@ -173,108 +278,23 @@ int main(int argc,char** argv)
 		cout<<"You are using "<<name<<" version "<<ver<<vermod<<endl;
 	if(!strcmp(mode,"em"))
 		em();
+        if(!strcmp(mode,"em"))
+		emd(argv[3]);
 	if(!strcmp(mode,"eml"))
-		{
+	{
 			if(!eMl(argv[2],argv[3])){
 				while(true)
 				return 0;
 			}
 				else {cout<<"There are some troubles in the system or program"<<endl; return 1;}
-		}
-        if(!strcmp(mode,"edl")||!strcmp(mode,"edml"))
-        {
-                strcpy(path,argv[2]);
-                limit=atol(argv[3]);
-		long back=limit;
-                delete[] buf;
-                strcpy(mult,argv[4]);
-       		 if(!strcmp(mult,"b"))
-                	limit*=1;
-       		if(!strcmp(mult,"k"))
-                	limit*=1024;
-       		if(!strcmp(mult,"m"))
-	                limit*=1024*1024;
-		 if(!strcmp(mult,"g"))
-	                limit*=1024*1024*1024;
-		limit=limit-back;
 	}
-	double start = clock ();
-	FILE *fp1 = fopen(path,"ab");
-	FILE *fp2 = fopen(strcat(path,"2"),"ab");
-	FILE *fp3 = fopen(strcat(path,"3"),"ab");
-        if((fp1&&fp2&&fp3))
+        if(!strcmp(mode,"edl"))
         {
-                while(true)
-                {
-
-                        if(!strcmp(mode,"ed")||strcmp(mode,"emd")||!strcmp(mode,"edl")||strcmp(mode,"emdl"))
-                        {
-                                long fSizeS=ftell(fp1)+ftell(fp2)+ftell(fp2);
-                                cout<<fSizeS<<" "<<limit<<endl;
-                               if((ftell(fp1)+ftell(fp2)+ftell(fp2))<limit)
-                                {
-                                        fseek(fp1,0,SEEK_END);
-                                       long size1 = ftell(fp1);
-                                        fseek(fp2,0,SEEK_END);
-                                        long size2 = ftell(fp2);
-                                         fseek(fp3,0,SEEK_END);
-                                        long size3 = ftell(fp3);
-                                        if(limit<=1073741824)
-                                        {
-                                        char *buffer = (char*)calloc(limit/3-size1,1);
-                                        fwrite(buffer,limit/3-size1,1,fp1);
-                                        delete[] buffer;
-                                        buffer = (char*)calloc(limit/3-size2,1);
-                                        fwrite(buffer,limit/3-size2,1,fp2);
-                                        delete[] buffer;
-                                        buffer = (char*)calloc(limit/3-size3,1);
-                                        fwrite(buffer,limit/3-size3,1,fp3);
-                                        delete[] buffer;
-                                        }                                    
-					if(limit>1073741824)
-                                        {
-                                        long int count=limit/1073741824;
-					count++;
-                                        limit/=count;
-                                        cout<<limit<<" "<<count<<endl;
-                                        for(int i=0;i<count;i++)
-                                        {
-                                        char *buffer = (char*)calloc(limit/3-size1,1);
-                                        fwrite(buffer,limit/3-size1,1,fp1);
-                                        delete[] buffer;
-                                        buffer = (char*)calloc(limit/3-size2,1);
-                                        fwrite(buffer,limit/3-size2,1,fp2);
-                                        delete[] buffer;
-                                        buffer = (char*)calloc(limit/3-size3,1);
-                                        fwrite(buffer,limit/3-size3,1,fp3);
-                                        delete[] buffer;
-                                        }
-                                        }
-                                	long fSizeF=ftell(fp1)+ftell(fp2)+ftell(fp2);
-                                	cout<<fSizeS<<" "<<limit<<endl;
-					if(fSizeF<fSizeS&&fSizeF<limit)
-					{
-						cerr<<"Error: Could now write file"<<endl;
-						return 0;
-					}
-                              		}
-                                	else
-                                	{
-                                        	break;
-                                	}
-                        	}
-               		};
-                	if(!strcmp(mode,"ed")||!strcmp(mode,"edl")||strcmp(mode,"emdl"))
-                      { 
-			fclose(fp1);
-			fclose(fp2);
-			fclose(fp3);
-			}
-        }
+            edl(argv[3],argv[4],argv[5]);
+	}
 	double stop = clock();
 	double total = (stop - start) / CLOCKS_PER_SEC;
         cout<<"Done for "<<total<<" sec"<<endl;
-        delete[] path;
         delete mode;
         return 0;
 }
