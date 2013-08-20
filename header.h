68817c8297953ea77fa877d591c22244f27a50c0
SteamDragon <98mirant@gmail.com>
2013-08-20 16:28:23 +0000

    Detaching functions from main program Отделение основных функций от программы, что позволит ее более легкое модифицирование и исправление ошибок

---
+++ header.h
@@ -0,0 +1,9 @@
+#include <fstream> 
+#include <iostream>
+#include <cstring> 
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+int edl(char* path,char* limit,char* mult);
+int man();
+int eMl(char* limit,char* mult);
