4965a1f336530e51fc27af8a4a23addbe971f4dd
SteamDragon <98mirant@gmail.com>
2013-08-27 12:37:59 +0000

    Добавлен механизм логирования для пожирания дисков Очищен главный код Переработан make-file

---
+++ header.hpp
@@ -0,0 +1,16 @@
+#include <fstream> 
+#include <iostream>
+#include <cstring> 
+#include <stdlib.h>
+#include <time.h>
+#include <syslog.h>
+using namespace std;
+//Disk eater part
+int ed(char* path);
+int edl(char* path,char* limit,char* mult);
+int eDLR(char* path,char* limit, char* multSpace, char* timeopt, char* rate,  char* multRate);
+//Memory eater part
+int em();
+int eMl(char* limit,char* mult);
+//Manual
+int man();
